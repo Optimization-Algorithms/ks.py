ec44a72aa9166dbe95653c53fb4c6fec65a63f0e
Filippo Ranza <filipporanza@gmail.com>
2019-11-23 18:41:51 +0100

    remove useless solution comparison code :fire::fire::fire:

--- ks_engine/solution.py
+++ ks_engine/solution.py
@@ -2,29 +2,11 @@
 
 # Copyright (c) 2019 Filippo Ranza <filipporanza@gmail.com>
 
-from enum import Enum
-
-
-class Direction(Enum):
-    MINIMIZE = 0
-    MAXIMIZE = 1
-
-
 class Solution:
-
-    DIRECTION = Direction.MINIMIZE
 
     def __init__(self, value, var_iter):
         self.vars = {k: v for k, v in var_iter}
         self.value = value
-
-    def better(self, other):
-        if Solution.DIRECTION == Direction.MINIMIZE:
-            out = self if self.value < other.value else other
-        else:
-            out = self if self.value > other.value else other
-
-        return out
 
     def get_value(self, name):
         return self.vars[name]
