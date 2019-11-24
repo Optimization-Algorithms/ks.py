4c79999a1e99f9a9d4c2ca2d47b6f43c750a0c93
Filippo Ranza <filipporanza@gmail.com>
2019-11-24 15:13:05 +0100

    add a new percentage kernel builder :sparkles::sparkles::sparkles:

--- ks_engine/kernel_utils/base_kernel.py
+++ ks_engine/kernel_utils/base_kernel.py
@@ -3,6 +3,21 @@
 # Copyright (c) 2019 Filippo Ranza <filipporanza@gmail.com>
 
 
+import math
 
 def base_kernel_builder(base, values):
     return base
+
+
+def sort_base_variables(base, values):
+    kernel = [name for name, select in base.items() if select]
+    kernel.sort(key=lambda x: values.get_value(x))
+    return kernel
+
+def percentage_better_kernel_builder(base, value, percentage):
+    kernel_vars = sort_base_variables(base, value)
+    amount = math.floor(len(kernel_vars) * percentage)
+    return kernel_vars[:amount]
+
+
+
---
+++ test/test_base_kernel.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+
+# Copyright (c) 2019 Filippo Ranza <filipporanza@gmail.com>
+
+
+import unittest 
+from string import ascii_lowercase
+
+from ks_engine.kernel_utils.base_kernel import sort_base_variables, percentage_better_kernel_builder
+from ks_engine.solution import Solution
+
+def build_kernel():
+
+    kernel = {k: i % 2 == 0 for i, k in enumerate(ascii_lowercase)}
+    values = Solution(0.0, ((k, -i) for i, k in enumerate(ascii_lowercase)))
+
+    return kernel, values
+
+
+class TestKernelSorter(unittest.TestCase):
+    def test_kernel_sort(self):
+        kernel, values = build_kernel()
+        sort_kernel = sort_base_variables(kernel, values)
+        self.assertEqual(len(ascii_lowercase) / 2, len(sort_kernel))
+        self.assertEqual(sort_kernel, ['y', 'w', 'u', 's', 'q', 'o', 'm', 'k', 'i', 'g', 'e', 'c', 'a'])
+
+
+class TestKernelBuilder(unittest.TestCase):
+    def test_kernel_builder(self):
+        kernel, values = build_kernel()
+        kernel = percentage_better_kernel_builder(kernel, values, 0.75)
+        # 13 * 0.75 = 9.75 -> 9
+        self.assertEqual(len(kernel), 9)
+        self.assertEqual(kernel, ['y', 'w', 'u', 's', 'q', 'o', 'm', 'k', 'i'])        
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
