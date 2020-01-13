8ff6b2d2a13373ba18725957875b00ad19919af1
Filippo Ranza <filipporanza@gmail.com>
2020-01-14 00:17:25 +0100

    add Debug info storage class, without debug collection

--- ks_engine/solution.py
+++ ks_engine/solution.py
@@ -2,11 +2,70 @@
 
 # Copyright (c) 2019 Filippo Ranza <filipporanza@gmail.com>
 
+from collections import namedtuple
+import gzip 
+
+DebugData = namedtuple("DebugData", ["value", "time", "nodes", "kernel_size", "bucket_size"])
+DebugIndex = namedtuple("DebugIndex", [ "iteration", "bucket"])
+
+
+class DebugInfo:
+    def __init__(self):
+        self.store = {}
+        self.max_bucket = 0
+        self.max_iter = 0
+
+    def add_data(self, data, index):
+        self.store[index] = data
+
+        if self.max_bucket < index.bucket:
+            self.max_bucket = index.bucket
+        
+        if self.max_iter < index.iteration:
+            self.max_iter = index.iteration
+
+    def export_csv(self, file_name, compress):
+        csv = self.get_csv()
+        if compress:
+            csv = bytes(csv, "UTF-8")
+            with gzip.open(file_name, "wb") as file:
+                file.write(csv)
+        else:
+            with open(file_name, "w") as file:
+                file.write(csv)
+ 
+    def get_csv(self):
+        out = "bucket, iteration, value, time, nodes, kernel_size, bucket_size"
+        for k, v in self.store.items():
+            tmp = f"{k.bucket}, {k.iteration}, {v.value}, {v.time}, {v.nodes}, {v.kernel_size}, {v.bucket_size}"
+            out += "\n" + tmp 
+        return out
+
+    def bucket_iter(self, iteration):
+        for k, v in self.store.items():
+            if k.iteration == iteration:
+                yield k.bucket, v
+
+    def iteration_iter(self, bucket):
+        for k, v in self.store.items():
+            if k.bucket == bucket:
+                yield k.iteration, v
+
+    def full_iter(self):
+        for k, v in self.store.items():
+            yield k, v
+
+
+
 
 class Solution:
-    def __init__(self, value, var_iter):
+    def __init__(self, value, var_iter, debug=False):
         self.vars = {k: v for k, v in var_iter}
         self.value = value
+        if debug:
+            self.debug = DebugInfo()
+        else:
+            self.debug = None
 
     def get_value(self, name):
         return self.vars[name]
@@ -15,3 +74,6 @@ class Solution:
         self.value = value
         for k, v in var_iter:
             self.vars[k] = v
+        
+    def update_debug_info(self, index, debug_info):
+        self.debug.add_data(debug_info, index)
---
+++ test/test_debug_data.py
@@ -0,0 +1,90 @@
+#! /usr/bin/python
+
+import unittest
+
+from secrets import randbelow
+from tempfile import TemporaryDirectory
+from os import path
+import gzip
+from ks_engine.solution import DebugInfo, DebugData, DebugIndex
+
+
+class BaseTest(unittest.TestCase):
+    def get_random_data(self, count):
+        out = []
+        for _ in range(count):
+            data = DebugData(
+                randbelow(100),
+                randbelow(100),
+                randbelow(100),
+                randbelow(100),
+                randbelow(100),
+            )
+            out.append(data)
+        return out
+
+    def test_storage(self):
+        buckets = 20
+        store = DebugInfo()
+        iter_one = self.get_random_data(buckets)
+        for i, d in enumerate(iter_one):
+            index = DebugIndex(0, i)
+            store.add_data(d, index)
+
+        iter_two = self.get_random_data(buckets)
+        for i, d in enumerate(iter_two):
+            index = DebugIndex(1, i)
+            store.add_data(d, index)
+
+        # test bucket iteration
+        for i in range(buckets):
+            data = [iter_one[i], iter_two[i]]
+            for k, v in store.iteration_iter(i):
+                self.assertEqual(data[k], v)
+
+        for k, v in store.bucket_iter(0):
+            self.assertEqual(iter_one[k], v)
+
+        for k, v in store.bucket_iter(1):
+            self.assertEqual(iter_two[k], v)
+
+    def build_base_export(self):
+        store = DebugInfo()
+
+        data = DebugData(1, 1, 1, 1, 1)
+        index = DebugIndex(0, 0)
+        store.add_data(data, index)
+
+        data = DebugData(1, 1, 1, 1, 1)
+        index = DebugIndex(0, 1)
+        store.add_data(data, index)
+
+        expected = "bucket, iteration, value, time, nodes, kernel_size, bucket_size\n0, 0, 1, 1, 1, 1, 1\n1, 0, 1, 1, 1, 1, 1"
+
+        return store, expected
+
+    def test_export(self):
+        store, expected = self.build_base_export()
+        csv = store.get_csv()
+        
+        self.assertEqual(csv, expected)
+
+    def test_file_export(self):
+        store, expected = self.build_base_export()
+        with TemporaryDirectory() as tmp_root:
+            file = path.join(tmp_root, "debug.csv")
+            store.export_csv(file, False)
+            with open(file, "r") as data:
+                self.assertEqual(data.read(), expected)
+
+    def test_compressed_file_export(self):
+        store, expected = self.build_base_export()
+        with TemporaryDirectory() as tmp_root:
+            file = path.join(tmp_root, "debug.csv.gz")
+            store.export_csv(file, True)
+            with gzip.open(file, "r") as data:
+                read = data.read().decode()
+                self.assertEqual(read, expected)
+
+if __name__ == "__main__":
+    unittest.main()
