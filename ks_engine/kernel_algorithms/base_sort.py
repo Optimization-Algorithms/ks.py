40b117105a0b74d9ff10dbc6f09694f093b24627
Filippo Ranza <filipporanza@gmail.com>
2020-08-10 16:25:42 +0200

    fix ndarray split

--- ks_engine/kernel_algorithms/base_sort.py
+++ ks_engine/kernel_algorithms/base_sort.py
@@ -9,14 +9,15 @@ import numpy as np
 # support function for cheb sort
 
 
-def cheb_nodes(n: int):
-    tmp = []
-    for num in range(1, n):
-        t = np.cos((2 * num - 1) * np.pi / (2 * n))
+def cheb_nodes(count: int):
+    output = [0] * count
+    for i, n in enumerate(range(1, count + 1)):
+        tmp = (((2 * n) - 1) / (2 * count)) * np.pi 
+        t = np.cos(tmp)
         if t < 0:
             t += 1
-        tmp.append(t)
-    return tmp
+        output[i] = t
+    return np.array(output)
 
 
 def kernel_sort(kernel: dict, values):
@@ -34,19 +35,19 @@ def bucket_sort(kernel: dict, values):
 def cheb_sort(kernel: dict, values):
     tmp = [k for k, v in kernel.items() if not v]
     tmp.sort(key=lambda x: -values.get_value(x))
-    np.split(tmp, 2)  # divide la lista in due sottoliste
-    sup_ls = cheb_nodes(len(tmp))  # crea i nodi di cheb con due subarray
-    np.split(sup_ls, 2)
-    sorted = []  # analisi dei nodi e riordino della lista
+    head, tail = np.split(tmp, 2) 
+    nodes = cheb_nodes(len(tmp)) 
+    head_nodes, tail_nodes = np.split(nodes, 2)
+    output = []
     j, k = 0, 0
-    for _ in range(1, len(tmp)):
-        if sup_ls[0][j] < sup_ls[1][k]:
-            sorted.append(sup_ls[1][k])
+    for _ in range(len(tmp)):
+        if head_nodes[j] < tail_nodes[k]:
+            output.append(head[k])
             k += 1
         else:
-            sorted.append(sup_ls[0][j])
+            output.append(tail[j])
             j += 1
-    return sorted
+    return output
 
 
 KERNEL_SORTERS = {
